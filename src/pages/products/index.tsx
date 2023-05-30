2686271924bfd999ce73d0c0fa5459f1faaf6dd3
신준혁 <oridori2705@naver.com>
2023-05-31 01:21:08 +0900

    주석 추가
    주석추가

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -6,6 +6,7 @@ import {GET_PRODUCTS,Products} from '../../graphql/produts'
 
 const Productlist =() =>{
     //useQuery로 데이터 요청
+    //현재 리액트쿼리버전업데이트로 타입스크립트 정의방식이 바뀌었다. graphqlFetcher에도 타입을 지정해줘야한다.
     const { data } = useQuery<Products>([QueryKeys.PRODUCTS], 
         () =>graphqlFetcher<Products>(GET_PRODUCTS));
     console.log(data);
--- src/queryClient.ts
+++ src/queryClient.ts
@@ -86,6 +86,7 @@ const BASE_URL = 'https://fakestoreapi.com'
 //     }
 // }
 
+//현재 리액트쿼리버전업데이트로 타입스크립트 정의방식이 바뀌었다. graphqlFetcher에도 타입을 지정해줘야한다.
 //react-query버전 업데이트에 따라서 <T>를 넣어주고 fetcher부분에도 타입을 지정해줘야한다.
 export const graphqlFetcher = <T>(query: RequestDocument, variables = {}) =>
   request<T>(BASE_URL, query, variables)
